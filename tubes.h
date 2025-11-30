c26488018962435df9c8983f2c6230b807fea521
Lutfi Adam <najwaanisa28@gmail.com>
2025-11-30 16:21:25 +0700

    masukin sebagaian header

--- tubes.h
+++ tubes.h
@@ -1,4 +1,11 @@
 #ifndef TUBES_H_INCLUDED
 #define TUBES_H_INCLUDED
+using namespace std;
 
+
+struct User {
+    string nama;
+    string password;
+    string role;
+};
 #endif // TUBES_H_INCLUDED
