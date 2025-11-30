76f18f46edd177628622ccd0c8bbebe8f0b8e826
NajwaAnisa <najwaanisa28@gmail.com>
2025-11-30 16:48:12 +0700

    edit lagi

--- tubes.h
+++ tubes.h
@@ -8,4 +8,15 @@ struct User {
     string password;
     string role;
 };
+struct UserElm {
+    User info;
+    adrUser next;
+    adrUser prev;
+
+    // Relasi user → playlist (1-to-N)
+    adrPlaylist listPlaylist;
+
+    // Relasi user → song (favorite/history) (N-to-M)
+    adrRel listFavSong;
+};
 #endif // TUBES_H_INCLUDED
